8aac234dc2ba0c1f6b65746e1048b78b31bbfc01
Anthony Chu <antchu@microsoft.com>
2020-11-15 14:29:08 -0800

    Remove Azure Functions artifacts

--- api/greet/index.js
+++
@@ -1,7 +0,0 @@
-const { functions } = require('../lib/swa');
-
-module.exports = functions.http()
-    .allow({ users: ["anthony"] })
-    .onInvoke(function (name) {
-        return `Hello ${name}!`;
-    });
--- api/negotiate/index.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken');
-const swa = require('../lib/swa');
-
-module.exports = async function (context, req) {
-    context.res.json(swa.realtime.generateNegotiatePayload());
-}
